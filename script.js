8c00e40ae9c2536aa18e6ade4ede6660e2a83dec
CaroKris <ckhilpisch@gmail.com>
2021-01-18 16:26:59 -0600

    moved datavalue to textarea

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 var todaysDate = moment().format('LL'); 
 var currentTime = moment().format('LT');
 
-console.log(currentTime);
-
-
-
-
 $("#currentDay").html(todaysDate);
 
 // create a fumction to show the date and time using moment
@@ -15,20 +10,17 @@ $("#currentDay").html(todaysDate);
 
 
 // function for saving items
-// $( ".saveBtn" ).click(function() {
-//     var timeValue = ($(this).attr("data-value"));
-//     timeValue = parseInt(timeValue);
+$(window).load(function() {
+    var timeValue = ($(this).attr("data-value"));
+    timeValue = parseInt(timeValue);
 
-//     if (timeValue === ) {
-//         $addClass (.present);
-//     } else if {
-//         (timeValue < ) {
-//             $addClass.("future");
-//         } else if {
-//             (timeValue >) {
-//                 $addClass.("past");
-//             }
-//         }
-//     }
-// });
-console.log(moment().format('LL'));
+    if (timeValue === currentTime) {
+        $("textarea").addClass(".present");
+    } else if 
+        (timeValue < currentTime) {
+            $("textarea").addClass(".future");
+    } else if 
+        (timeValue > currentTime) {
+            $("textarea").addClass(".past");
+    }
+})
