9038faaba26cbd2caa28d53b692ddac813a1ada5
CaroKris <ckhilpisch@gmail.com>
2021-01-18 19:28:17 -0600

    adding the js to change the class of the textarea

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+$(document).ready(function() {
 var todaysDate = moment().format('LL'); 
-var currentTime = moment().format('LT');
+var currentTime = moment().hour();
+timeBlock = $(".time-block");
+textArea = $("textarea");
+
+
 
 $("#currentDay").html(todaysDate);
+
 
 // create a fumction to show the date and time using moment
 // use function to determine a class of either past, present, or future
@@ -10,17 +16,22 @@ $("#currentDay").html(todaysDate);
 
 
 // function for saving items
-$(window).load(function() {
-    var timeValue = ($(this).attr("data-value"));
+$(window).on("load",function() {
+    var timeValue = ($(textArea).attr("data-value"));
     timeValue = parseInt(timeValue);
 
     if (timeValue === currentTime) {
-        $("textarea").addClass(".present");
+        $("textArea").addClass(".present");
     } else if 
         (timeValue < currentTime) {
-            $("textarea").addClass(".future");
+            $("textArea").addClass(".future");
     } else if 
         (timeValue > currentTime) {
-            $("textarea").addClass(".past");
+            $("textArea").addClass(".past");
     }
+    console.log(timeValue);
 })
+
+
+console.log(currentTime);
+});
