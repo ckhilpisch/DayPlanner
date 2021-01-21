01fe1a80eda317a2fbc2fb5bbc3976941dfb3124
CaroKris <ckhilpisch@gmail.com>
2021-01-21 13:15:03 -0600

    able to move the data to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 var todaysDate = moment().format('LL'); 
 var currentTime = moment().hour();
-var textArea = $("textarea");
+var $textArea = $("textarea");
 
 console.log(currentTime);
 
@@ -10,36 +10,40 @@ $("#currentDay").html(todaysDate);
 // create a fumction to show the date and time using moment
 // use function to determine a class of either past, present, or future
 function classAddition() {
-    var timeValue = ($(textArea).attr("data-value"));
-    timeValue = parseInt(timeValue);
+    $textArea.each(function(){
 
-    if (timeValue === currentTime) {
-        $("textArea").addClass("present");
+    $timeValue = parseInt($(this).attr('data-value'));
+
+    if ($timeValue === currentTime) {
+        $(this).addClass("present");
     } else if 
-        (timeValue > currentTime) {
-            $("textArea").addClass("future");
+        ($timeValue > currentTime) {
+            $(this).addClass("future");
     } else if 
-        (timeValue < currentTime) {
-            $("textArea").addClass("past");
+        ($timeValue < currentTime) {
+            $(this).addClass("past");
     }
-    console.log(timeValue);
-}
+    // console.log($timeValue);
+    });
+};
 classAddition();
 
 // create afunction to save info to local storage using info
 
 $(".saveBtn").on("click", function () {
-    timeOfEvent = $(this).parent().attr("id");
-    // textInfo = $(this).prev().val().trim();
+    $timeOfEvent = $(this).parent().attr("id");
+    $textInfo = $(this).prev().val().trim();
 
-    // localStorage.setItem(timeOfEvent, textInfo);
+    localStorage.setItem($timeOfEvent, $textInfo);
     // console.log(timeOfEvent, textInfo);
   
-    console.log($(this));
-    console.log($(this).prev().val().trim());
+    // console.log($(this));
+    // console.log($(this).prev().val().trim());
     
-    console.log(timeOfEvent);
+    // console.log($timeOfEvent);
 
+  localStorage.getItem($textInfo);
+ 
 })
 
 
